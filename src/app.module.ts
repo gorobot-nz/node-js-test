f8914e3765d54a7abcefe1631ad20b567b91a202
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-05-05 11:53:13 +0300

    Add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "./user/user.module";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-    imports: [UserModule]
+    imports: [
+        UserModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.PG_HOST,
+            port: Number(process.env.PG_PORT),
+            username: process.env.PG_USER,
+            password: process.env.PG_PASSWORD,
+            database: process.env.PG_DB,
+            models: [],
+            autoLoadModels: true
+        }),
+    ]
 })
-export class AppModule{
+export class AppModule {
 
 }
