970e5c6afb26e67a2d74c0c65ce61c400ebd7b19
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-05-05 12:48:49 +0300

    Create user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { UserModule } from "./user/user.module";
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config'
+import { User } from "./user/user.model";
 
 @Module({
     imports: [
@@ -16,7 +17,7 @@ import { ConfigModule } from '@nestjs/config'
             username: process.env.PG_USER,
             password: process.env.PG_PASSWORD,
             database: process.env.PG_DB,
-            models: [],
+            models: [User],
             autoLoadModels: true
         }),
     ]
---
+++ src/user/user.model.ts
@@ -0,0 +1,33 @@
+import { Model, Table, Column, DataType } from 'sequelize-typescript'
+
+interface UserCreationAttributes {
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    image: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttributes>{
+    @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
+    id: number;
+
+    @Column({ type: DataType.STRING, unique: true, allowNull: false })
+    username: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    password: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    firstName: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    lastName: string;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    image: string;
+
+    @Column({ type: DataType.BLOB, allowNull: true })
+    pdf: BinaryData;
+}
